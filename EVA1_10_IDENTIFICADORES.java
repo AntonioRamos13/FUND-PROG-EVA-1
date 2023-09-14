ca7d2c906b1fa603f4d1086b8bcce37ec9c0abf6
AntonioRamos13 <143569091+AntonioRamos13@users.noreply.github.com>
2023-09-13 20:44:16 -0500

    Add files via upload

---
+++ EVA1_10_IDENTIFICADORES.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_10_IDENTIFICADORES {
+ /**
+  * 
+  */   
+public static void main (String[] args){
+    // TODO code application logic here
+    //int 1salario; no cumple las reglas: inicia con numero
+    //int salario&; no cumple: & no es caracter alfanumerico
+    int mi_salario;
+    int salario;
+    int salario1000;
+    //int_; no es valido
+    int $;
+    //int mi salario; no valido , epacio en blanco
+    
+    int SALARIO;
+    int Salario;
+    //int salario; no podemos repetir identificadores
+    
+    //int publi; no podemos usar keywords (palabras reservadas)
+                    
+}    int println
+}

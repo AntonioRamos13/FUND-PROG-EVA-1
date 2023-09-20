5ea1a334edcd663d2f2d81839f90c67ca03cac80
AntonioRamos13 <143569091+AntonioRamos13@users.noreply.github.com>
2023-09-19 20:52:14 -0500

    Add files via upload

---
+++ EVA_11_INICIALIZACION.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva_11_inicializacion;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA_11_INICIALIZACION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo");
+        int valor = 0; 
+        System.out.println(valor);
+        
+        //decalaramos e inicializamos variables dobles
+        double salario = 0, iva 0.16, subtotal = 10000;
+                
+        final int CALIFA_PASAR = 70;
+        final String ESCUELA = ("Instituto Tecnologico de Chihuahua II"); 
+        
+        System.out.println(ESCUELA);
+        System.out.println("La califa minima para pasar es:");
+        System.out.println(CALIFA_PASAR);
+        
+        int x;
+        byte valores = 120;
+        int miValor = Integer.MAX_VALUE;
+        System.out.println(miValor + 1);
+    }
+    
+}

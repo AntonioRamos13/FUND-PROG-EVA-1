ab52877f3818b2ee6e0ea86170d44ec74da95700
AntonioRamos13 <143569091+AntonioRamos13@users.noreply.github.com>
2023-11-08 20:55:11 -0600

    Add files via upload

---
+++ EVA2_23_ARREGLOS.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_23_arreglos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA2_23_ARREGLOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        int[] arreglo = new int[10]; 
+        arreglo[0] = 100;
+        arreglo[1] = 200;
+        arreglo[2] = 300;
+        arreglo[3] = 400; 
+          System.out.println("arreglo[0]");  
+          String[] arregloCade = new String[5];
+          arregloCade[0] = "Hola";
+          arregloCade[1] = " ";
+          arregloCade[2] = "mundo";
+          arregloCade[3] = " ";
+          arregloCade[4] = "cruel";
+          System.out.println("arregloCade*[0]");
+          System.out.println("arregloCade*[1]");
+          System.out.println("arregloCade*[2]");
+          System.out.println("arregloCade*[3]");
+          System.out.println("arregloCade*[4]");
+          arregloCade = new String[10];
+    }
+}
---
+++ EVA2_24_ARREGLOS_CICLOS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_24_arreglos_ciclos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA2_24_ARREGLOS_CICLOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int[] arreglo = new int[1000000];
+        for(int i = 0; i < 1000000; i++)
+            arreglo[i] = 10000;
+        System.out.println("arreglo[i]");
+    }
+}
